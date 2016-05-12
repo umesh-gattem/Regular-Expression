b331c4e9caac9fe6a388a5aeab6a8984b5ac95dc
Umesh Kumar Gattem <umesh.gattem@gmail.com>
2016-05-12 11:43:12 +0530

    Some changes are made in the program. Java format has given and documentation has given to every declaration.

--- src/com/regex/example/ExampleOne.java
+++ src/com/regex/example/ExampleOne.java
@@ -5,22 +5,34 @@ import java.util.regex.Pattern;
 
 /*Write regex to match a word in any line
  * Here is the code in Java to get the selected word using regex .
- * It simply displays the occurence of the word its starting index and ending index
+ * It simply displays the occurrence of the word its starting index and ending index
  * @author  Umesh Kumar
  * @since   11-05-2016 
  */
 public class ExampleOne {
+	public static Pattern patternMatchTheWord = Pattern.compile("my", Pattern.CASE_INSENSITIVE);
+
 	public static void main(String args[]) {
-		String line = "My name is umesh kumar" + "i have completed my b.tech in the year 2016"
-				+ "I am doing work on regular expression" +"My hobbies are playing cricket and listening music";
-		String pattern = "my";
-		Pattern p = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
-		Matcher m = p.matcher(line);
-		int count=0;
+		String line = "My name is umesh kumar\n" + "I have completed my b.tech in the year 2016\n"
+				+ "I am doing work on regular expression\n" + "My hobbies are playing cricket and listening music\n";
+		printPatternMatchTheWord(line);
+	}
+
+	/**
+	 * Declaration to display the word matching with the regex. It displays the
+	 * output as count(no of times the word has occurred), starting index of
+	 * matching word and its ending index and the matching word.
+	 * 
+	 * @param line
+	 */
+	private static void printPatternMatchTheWord(String line) {
+		// TODO Auto-generated method stub
+		Matcher m = patternMatchTheWord.matcher(line);
+		int count = 0;
 		while (m.find()) {
-			System.out.println("Count is:" +count++);
+			System.out.println("Count is:" + count++);
 			System.out.print("Start index: " + m.start());
-			System.out.print("\nEnd index: " + m.end() + " ");
+			System.out.print("\nEnd index: " + m.end());
 			System.out.println("\nPattern is: " + m.group());
 			System.out.println("\n");
 		}
